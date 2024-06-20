5fce6151ea0c7bf2b91b4fdc19d94bfaadaab525
jontygrbac <s3659145@student.rmit.edu.au>
2024-06-20 15:37:47 +1000

    Fix where there was more than one unique occurence

--- src/components/Letter.js
+++ src/components/Letter.js
@@ -9,19 +9,28 @@ function Letter({ letterPosition, attemptVal }) {
     setDisabledLetters,
     setCorrectLetters,
     setAlmostLetters,
+    correctLetters,
   } = useContext(AppContext);
   const letter = board[attemptVal][letterPosition];
 
-  //If position on word is the same as the cell
+  // Function to count occurrences of a letter in a word
+  const countOccurrences = (word, char) => {
+    return word.split('').filter(c => c === char).length;
+  };
+
+  // If position on word is the same as the cell
   const correct = correctWord[letterPosition] === letter;
 
-  //If it isn't correct, isn't black and is included in the word
+  // If it isn't correct, isn't black and is included in the word
   const almost = !correct && letter !== "" && correctWord.includes(letter);
 
-  //Determining the id, will only be allowed when attemptVal has reached it's max
+  // Check if the letter is correct and if there is only one occurrence of the letter in the correct word
+  const isUniqueCorrect = almost && countOccurrences(correctWord, letter) == 1 && correctLetters.includes(letter);
+
+  // Determining the id, will only be allowed when attemptVal has reached its max
   const letterState =
     currAttempt.attempt > attemptVal &&
-    (correct ? "correct" : almost ? "almost" : "error");
+    (isUniqueCorrect ? "error" : correct ? "correct" : almost ? "almost" : "error");
 
   useEffect(() => {
     if (letter !== "" && !correct && !almost) {
